23e34fa020e2e6bc6198640e897d78faea033b8f
Nikolay Stoitsev <nstoitsev@gmail.com>
2012-05-19 18:04:14 +0300

    add sidebar and fix the position of elements

--- application/views/header.php
+++ application/views/header.php
@@ -2,25 +2,26 @@
 
 <div class="navbar">
 	<div class="navbar-inner">
-	  <div class="container">
-	  	<a class="brand" href="#">Sinefy</a>
-	    <div class="nav-collapse">
-	    </div><!--/.nav-collapse -->
-	    <div class="btn-group pull-right open">
-	       <?php if ($current_user AND $current_user->loaded()):?>
-	  <div class="btn-group pull-right">
-        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-          <i class="icon-user"></i> <?php echo $current_user->name?>
-          <span class="caret"></span>
-        </a>
-        <ul class="dropdown-menu">
-          <li><?php echo HTML::anchor(Route::url('logout'), 'Log out')?></li>
-        </ul>
-      </div>
-      <?php else:?>
-  	<div class="fb-login-button" data-scope="email,user_likes,friends_likes" data-show-faces="true" data-width="200" data-max-rows="1"></div>
-      <?php endif;?>
-	    </div>
-	  </div>
+		<div class="container">
+			<a class="brand" href="#">Sinefy</a>
+			<div class="nav-collapse">
+			</div>
+			<!--/.nav-collapse -->
+			<div class="btn-group pull-right open">
+				<?php if ($current_user AND $current_user->loaded()):?>
+				<div class="btn-group pull-right">
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+					<i class="icon-user"></i> <?php echo $current_user->name?>
+					<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><?php echo HTML::anchor(Route::url('logout'), 'Log out')?></li>
+					</ul>
+				</div>
+				<?php else:?>
+				<div class="fb-login-button" data-scope="email,user_likes,friends_likes" data-show-faces="true" data-width="200" data-max-rows="1"></div>
+				<?php endif;?>
+			</div>
+		</div>
 	</div>
-  </div>
+</div>
