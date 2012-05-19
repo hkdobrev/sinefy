4de5388a5f6690df443c5e3fac561452ae15fde8
Nikolay Stoitsev <nstoitsev@gmail.com>
2012-05-19 18:35:01 +0300

    chage position of login button and improve control over what user can see

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,13 +1,23 @@
 <?php defined('SYSPATH') OR die('No direct script access.');?>
 
-<div class="row">
+<div class="row-fluid">
+  <div class="span9">
+      <?php if ($current_user AND $current_user->loaded()):?> 
+        <!-- grid -->   
+       <?php else: ?>
+         <h1>Want to watch a movie?</h1>
+         <div class="fb-login-button" data-scope="email,user_likes,friends_likes" data-show-faces="true" data-width="200" data-max-rows="1"></div>
+      <?php endif; ?>      
+  </div>
 
-      <div style="width:250px; float: right;" class="sidebar">
-          <div style="widht: 100%; height: 300px;"></div>
-      </div>
-
-      <div >
-          <div style="widht: 100%; height: 300px;"></div>
-      </div>
+  <div class="span3">
+    <div class="sidebar">
+      <?php if ($current_user AND $current_user->loaded()):?>     
+        <h3>Click on a movie to see details</h3>
+      <?php else: ?>
+        <h3>Sign in and you will be able to:</h3>
+      <?php endif; ?>
+    </div>
+  </div>
 
 </div>
