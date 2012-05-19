4feba6ef3ec27577c4f4840ad2fd1799b9b2d94e
Nikolay Stoitsev <nstoitsev@gmail.com>
2012-05-19 22:07:03 +0300

    resolve conflicts

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');?>
 
 <div class="row-fluid">
-<<<<<<< Updated upstream
 	<div class="span9">
 	<?php if ($current_user AND $current_user->loaded()):?> 
 	<ul id="posters">
@@ -38,36 +37,18 @@
 	<?php endif; ?>      
 	</div>
 
-	<div class="span3">
-=======
-  <div class="span9">
-      <?php if ($current_user AND $current_user->loaded()):?> 
-        grid 
-       <?php else: ?>
-         <h1>Want to watch a movie?</h1>
-         <div class="fb-login-button" data-scope="email,user_likes,friends_likes" data-show-faces="true" data-width="200" data-max-rows="1"></div>
-      <?php endif; ?>      
-  </div>
-
   <div class="span3">
-    <div class="sidebar">
-      <?php if ($current_user AND $current_user->loaded()):?>     
-        <h2>Avengers</h2>
-
-        <img src="http://img.youtube.com/vi/NPoHPNeU9fc/0.jpg" class="trailer-preview" />
-      <?php else: ?>
-        <h3>Sign in and you will be able to:</h3>
-      <?php endif; ?>
-    </div>
-  </div>
->>>>>>> Stashed changes
-
 		<div class="sidebar">
 			<?php if ($current_user AND $current_user->loaded()):?>     
 				<h3>Click on a movie to see details</h3>
 				<?php foreach ($movies_users->rewind() as $movies_user):
 				$movie = $movies_user->movie;?>
 				<div id="movie-<?php echo $movie->pk()?>" class="movie-sidebar">
+
+          <h2>Avengers</h2>
+
+          <img src="http://img.youtube.com/vi/NPoHPNeU9fc/0.jpg" class="trailer-preview" />
+
 				</div>
 				<?php endforeach?>
 			<?php else: ?>
--- modules/youtube/classes/youtube/playlist/video.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class YouTube_Playlist_Video extends YouTube_Playlist_Video_Data {
-    
-}
