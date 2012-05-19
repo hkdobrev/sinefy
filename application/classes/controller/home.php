07818c84afe9249b5cd03fe7336774b6032d635d
Stoyan Dipchikov <sdipchikov@despark.com>
2012-05-19 20:03:09 +0300

    removed debug line from home contr

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -4,8 +4,7 @@ class Controller_Home extends Controller_Layout {
 
 	public function action_index()
 	{
-		$user_movies = ORM::factory('user', 1)->movies->where('movie.id', '=', 2)->wishlist();
-		echo "<pre>"; var_dump($user_movies); die;
+		
 	}
 
 } 
