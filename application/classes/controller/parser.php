22d0210fe3eb3527b1064f260b11b67f45720fdc
Stoyan Dipchikov <sdipchikov@despark.com>
2012-05-19 21:45:48 +0300

    movie data

--- application/classes/controller/parser.php
+++ application/classes/controller/parser.php
@@ -1,1 +1,53 @@
-movie_parser.php
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Parser extends Controller {
+	
+	public function action_index()
+	{
+		$tokens = array("hkdobrev" => 'AAAAAAITEghMBAL14SZCNKLnuSusvNRWZCZCO1T7PiNko8masrcTARCDcWV0LcyXJRsXgn8snRhppG22dA8RAkXIErEqZC55bucupirHCfYPYvJAZC79hA',
+						"1082926232" => 'AAAAAAITEghMBAMApJoXRnu6H8gLoxTR1sQxeLxFMIURjGkj39D6ZCmhKx5L9ZAVavPhZAIQADkp5EWzDNNSeeawuKoSZBQN5jdAChN24fQ9ebyp1qAtl',
+						"nstoitsev" => "AAAAAAITEghMBAFTPfY6y0taHyNVcGgBDzkUDo8Y7uZBDSSmHLQJhMaer53ObNqbKcq2LcjrS6HsN0qhwzhsHP3iwruygjV3K1Nw37eAZDZD");
+
+		
+		Kohana::load(Kohana::find_file('vendor/facebook-php-sdk', 'facebook'));
+
+		$facebook = new Facebook(array(
+			'appId'  => Kohana::$config->load('facebook.app_id'),
+			'secret' => Kohana::$config->load('facebook.app_secret'),
+		));
+		
+		foreach ($tokens as $user_id => $token) 
+		{
+			$facebook->setAccessToken($token);
+
+
+			$user_movies = $facebook->api('me/movies');
+
+			// echo "<pre>"; print_r($user_movies); die;
+			
+			foreach($user_movies['data'] as $movie)
+			{
+				$movie_json = Request::factory('http://graph.facebook.com/' . $movie['id'])
+										->execute()
+										->body();
+
+				$movie_data = json_decode($movie_json, true);
+
+				// echo "<pre>"; print_r($movie_data); die;
+
+				$new_movie = ORM::factory('movie')
+								->values(array(
+											'name' => Arr::get($movie_data, 'name'),
+											'about' => Arr::get($movie_data, 'description', Arr::get($movie_data, 'about')),
+											'starring' => Arr::get($movie_data, 'starring'),
+											'release_date' => Arr::get($movie_data, 'release_date'),
+											'directed_by' => Arr::get($movie_data, 'directed_by'),
+											'studio' => Arr::get($movie_data, 'studio'),
+											'likes' => Arr::get($movie_data, 'likes'),
+											'id' => Arr::get($movie_data, 'id'),
+										))
+								->save();
+			}
+		}
+	}
+}
