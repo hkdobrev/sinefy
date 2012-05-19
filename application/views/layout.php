5d8f8ccf26cfbf0c6deba711794b5e962ffa6c18
Nikolay Stoitsev <nstoitsev@gmail.com>
2012-05-19 14:45:42 +0300

    add new layout

--- application/views/layout.php
+++ application/views/layout.php
@@ -17,27 +17,30 @@
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
-	<div class="navbar navbar-fixed-top">
-	  <div class="navbar-inner">
-		<div class="container">
-		  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </a>
-		  <a class="brand" href="#">SocialMovies</a>
-		  <div class="nav-collapse">
-			<ul class="nav">
-			  <li class="active"><a href="#">Home</a></li>
-			  <li><a href="#about">About</a></li>
-			  <li><a href="#contact">Contact</a></li>
-			</ul>
-		  </div><!--/.nav-collapse -->
+	<div class="container">
+
+	  <div class="header">
+
+	  </div>
+
+	  <div class="navbar">
+		<div class="navbar-inner">
+		  <div class="container">
+		    <div class="nav-collapse">
+		      <ul class="nav">
+		        <li class="active"><a href="#">Your Profile</a></li>
+		        <li><a href="#about">About</a></li>
+		        <li><a href="#contact">Contact</a></li>
+		      </ul>
+		    </div><!--/.nav-collapse -->
+		    <div class="btn-group pull-right open">
+		      <a class="btn" href="#">
+		        <i class="icon-user"></i> Login
+		      </a>
+		    </div>
+		  </div>
 		</div>
 	  </div>
-	</div>
-
-	<div class="container">
 
 	  <?php echo $content;?>
 
@@ -53,3 +56,4 @@
 <?php echo View::factory('assets/body')?>
 </body>
 </html>
+
