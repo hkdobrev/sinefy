5a5a6df20bc178720c41eea72784fbe6a89782d0
Nikolay Stoitsev <nstoitsev@gmail.com>
2012-05-20 10:58:12 +0300

    merge

--- application/views/assets/body.php
+++ application/views/assets/body.php
@@ -1,11 +1,11 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 $assets = Assets::factory('body')
+	->js('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js')
 	->js('libs/bootstrap/bootstrap.min.js')
 	->js('plugins.js')
 	->js('libs/jquery.masonry.min.js')
-	->js('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js')
-	->js('libs/jquery.infinitescroll.min.js')
+	->js('libs/jquery.infinitescroll.js')
 	->js('script.js')
 	->js('sidebar.js');
 
