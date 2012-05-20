ee663df73bf79a666067ebbb5013c19c3594ea46
Haralan Dobrev <hkdobrev@gmail.com>
2012-05-20 10:35:55 +0300

    modified infite scroll and sidebar.js to show info for later loaded content

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -6,7 +6,7 @@ return array(
 	'default' => array(
 		'current_page'      => array('source' => 'route', 'key' => 'page'), // source: "query_string" or "route"
 		'total_items'       => 0,
-		'items_per_page'    => 25,
+		'items_per_page'    => 15,
 		'view'              => 'pagination/load_more',
 		'auto_hide'         => FALSE,
 		'first_page_in_url' => FALSE,
