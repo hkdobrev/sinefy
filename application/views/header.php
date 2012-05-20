42095651aab1e212fb95e98edfb8270fe18ce7d4
Stoyan Dipchikov <sdipchikov@despark.com>
2012-05-20 15:32:28 +0300

    header

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
 <div class="navbar">
 	<div class="navbar-inner">
 		<div class="container">
-			<a class="brand" href="#">Sinefy</a>
+			<a class="brand" href="#"><img src="assets/img/logo.png" style="margin-top: -7px;" width="100px"></a>
 			<?php if ($current_user AND $current_user->loaded()):?>
 				<div class="btn-group pull-right open">
 					<div class="btn-group pull-right">
